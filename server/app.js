47aa3fa091746b1df20b6e804c2625a82302212e
David Espinal <despinal0425@gmail.com>
2021-08-14 13:24:28 -0400

    completed te app.js

---
+++ server/app.js
@@ -0,0 +1,25 @@
+//App
+//framework for server
+const express = require("express");
+
+//specific local host have access to the server
+const cors = require("cors");
+
+//logs request and morgan happens to be a middleware for logger. it helps generate request logs
+const logger = require("morgan");
+
+const itemRoutes = require("./routes/items");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(logger("dev"));
+
+app.use(cors());
+
+app.get("/", (req, res) => res.send("API is running!"));
+
+app.use("/api", itemRoutes);
